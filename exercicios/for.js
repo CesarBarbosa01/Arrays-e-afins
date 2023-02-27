ea41a7b645792fc9ea1b578920d227bc5edb7573
César Barbosa <edinaldocesar14@gmail.com>
2023-02-27 19:12:12 -0300

    for

---
+++ exercicios/for.js
@@ -0,0 +1,7 @@
+/* 
+Exercício Estrutura de Repetição - For
+- Faça uma tabuada de 10.
+*/
+    for (var n1 = 0; n1 <= 10; n1++) {
+        console.log(`A tabuada de 10 x ${n1} é: ${n1 * 10}.`)
+    }
