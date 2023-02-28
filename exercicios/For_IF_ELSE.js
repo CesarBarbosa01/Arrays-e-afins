0f660b7a47536e94cf63ccb929cf7fedaea2a7a5
César Barbosa <edinaldocesar14@gmail.com>
2023-02-28 16:40:31 -0300

    nothing

---
+++ exercicios/For_IF_ELSE.js
@@ -0,0 +1,6 @@
+/* 
+Faça um array que contenha 5 números, e com a estrutura de repetição
+FOR, e utilizando o IF/ELSE diga qual é o maior número do array.
+
+NÃO FUI CAPAZ DE RESOLVER. DOR...
+*/
--- exercicios/forTabuada10.js
+++ exercicios/forTabuada10.js
@@ -2,6 +2,6 @@
 Exercício Estrutura de Repetição - For
 - Faça uma tabuada de 10.
 */
-    for (var n1 = 0; n1 <= 10; n1++) {
+    for (let n1 = 0; n1 <= 10; n1++) {
         console.log(`A tabuada de 10 x ${n1} é: ${n1 * 10}.`)
     }
