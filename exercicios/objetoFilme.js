636553c40e007dd696172a055e33a06fb40e730a
César Barbosa <edinaldocesar14@gmail.com>
2023-02-28 00:37:42 -0300

    nova adição

---
+++ exercicios/objetoFilme.js
@@ -0,0 +1,7 @@
+let filme = {
+    nome: "O homem que copiava",
+    genero: "Romance/Drama",
+    nota: "9,9"
+}
+
+console.log(`O filme "${filme.nome}" é um ${filme.genero}. Um marco do cinema brasileiro com nota de ${filme.nota}.`)
