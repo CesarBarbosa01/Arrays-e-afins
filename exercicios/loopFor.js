64f7b24c39aa14710b91d1e43ec060c91c2e197d
César Barbosa <edinaldocesar14@gmail.com>
2023-02-28 21:53:28 -0300

    atualizações

--- exercicios/loopFor.js
+++ exercicios/loopFor.js
@@ -1,2 +1,15 @@
+/* 
+Faça um programa com o loop FOR, que some todos os valores do loop, e
+ao final mostre a média desses valores, e a soma
 
+OBS: provavelmente tá erradokkkkk
+     mas fiz.
+*/
 
+for (var n1 = 1; n1 <= 5; n1++) {
+     var soma = n1 + n1
+     var media = soma / 2
+    console.log(`A soma entre ${n1} e ${n1} é ${soma}`)
+    console.log(`A média dos números (${n1}, ${n1}) é ${media}.`)
+
+}
